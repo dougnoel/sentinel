1938ee2c7c5733d9abd098f940d7bba3e0665b0d
dougnoel <dougnoel@gmail.com>
2020-05-03 23:15:46 -0400

    Added SauceLabsDriverFatory to further refactor the code. Removed a couple of unused methods.

--- src/main/java/com/dougnoel/sentinel/webdrivers/WebDriverFactory.java
+++ src/main/java/com/dougnoel/sentinel/webdrivers/WebDriverFactory.java
@@ -1,18 +1,15 @@
 package com.dougnoel.sentinel.webdrivers;
 
-import java.net.URL;
 import java.util.HashMap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.dougnoel.sentinel.configurations.ConfigurationManager;
@@ -64,102 +61,8 @@ public class WebDriverFactory {
 
     private static WebDriverFactory instance = null;
 
-    private static String downloadPath = "../downloads";
-
     protected WebDriverFactory() {
         // Exists only to defeat instantiation.
-    }
-    
-    /**
-     * Creates a single threaded Saucelabs WebDriver.
-     * @return WebDriver a Saucelabs WebDriver
-     * @throws ConfigurationNotFoundException if a requested configuration property has not been set
-     */
-    private static WebDriver createSaucelabsDriver() throws ConfigurationNotFoundException {
-        URL SAUCELABS_URL;
-		try {
-			SAUCELABS_URL = new URL("https://ondemand.saucelabs.com:443/wd/hub");
-		} catch (java.net.MalformedURLException e) {
-			throw new MalformedURLException(e);
-		}
-        
-		String browser = ConfigurationManager.getProperty("browser");
-		String operatingSystem = ConfigurationManager.getProperty("os");
-		
-        MutableCapabilities options = new MutableCapabilities();
-        options.setCapability("platform", operatingSystem);
-        options.setCapability("browserName", browser);
-        String browserVersion = ConfigurationManager.getOptionalProperty("browserVersion");
-        if (browserVersion != null) {
-        	options.setCapability("version", browserVersion);
-        } else {
-        	options.setCapability("version", "latest");
-        }
-        
-        options.setCapability("username", ConfigurationManager.getProperty("saucelabsUserName"));
-        options.setCapability("accesskey", ConfigurationManager.getProperty("saucelabsAccessKey"));
-        
-        options = setSaucelabsTestNameProperty(options);
-        
-        options = setOptionalSaucelabsProperty("parent-tunnel", options);
-        options = setOptionalSaucelabsProperty("tunnelIdentifier", options);
-        options = setOptionalSaucelabsProperty("tags", options);
-        options = setOptionalSaucelabsProperty("build", options);
-        
-        RemoteWebDriver driver = new RemoteWebDriver(SAUCELABS_URL, options);
-        
-        return driver;
-    }
-    
-    /**
-     * Looks for a passed Saucelabs property name and sets it if it exists.
-     * @param saucelabsPropertyName String the property to be set if it exists
-     * @param options MutableCapabilities the MutableCapabilities object in which to set the properties
-     * @return MutableCapabilities the new MutableCapabilities object with the properties set.
-     */
-    private static MutableCapabilities setOptionalSaucelabsProperty(String saucelabsPropertyName, MutableCapabilities options) {
-        String saucelabsProperty = ConfigurationManager.getOptionalProperty(saucelabsPropertyName);
-        
-        if (StringUtils.isNotEmpty(saucelabsProperty)) {
-        	options.setCapability(saucelabsPropertyName, saucelabsProperty);
-        }
-        
-    	return options;
-    }
-    
-    /**
-     * Pulls config values and sets a string to pass to Saucelabs for the job.
-     * @param options MutableCapabilities object to modify
-     * @return MutableCapabilities modified MutableCapabilities object
-     */
-    private static MutableCapabilities setSaucelabsTestNameProperty(MutableCapabilities options)
-    {
-        String testName = "";
-        String jobName = ConfigurationManager.getOptionalProperty("name");
-        if (jobName != null) {
-        	testName += "Name: " + jobName + " ";
-        }
-        String tags = ConfigurationManager.getOptionalProperty("tags");
-        if (tags != null) {
-        	testName += "Tags: " + tags + " ";
-        }
-        String userName = System.getProperty("user.name");
-        if (userName != null) {
-        	testName += "User: " + userName + " ";
-        }
-        String build = ConfigurationManager.getOptionalProperty("build");
-        if (build != null) {
-        	testName += "Build: " + build + " ";
-        }
-        if (testName.isEmpty()) {
-        	testName += "Default Sentinel Test Name";
-        }
-        else {
-        	testName = testName.stripTrailing();
-        }
-        
-        options.setCapability("name", testName);
-        return options;
     }
     
     //TODO: Add all of the valid browser options and make them match saucelabs options
@@ -186,7 +89,7 @@ public class WebDriverFactory {
         //Saucelabs Driver setup
         String saucelabsUserName = ConfigurationManager.getOptionalProperty("saucelabsUserName");
         if (saucelabsUserName != null) {
-        	return driver = createSaucelabsDriver(); //NOTE: Returning the driver here so that we do not need an extra else statement.
+        	return driver = SauceLabsDriverFactory.createSaucelabsDriver(); //NOTE: Returning the driver here so that we do not need an extra else statement.
         }
 
         // Set a Download Directory if one was specified on the command line
@@ -240,26 +143,6 @@ public class WebDriverFactory {
         chromePrefs.put("download.default_directory", filePath);
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", chromePrefs);
-    }
-
-    /**
-     * Returns the download path
-     * 
-     * @return downloadPath String the file path to the download directory
-     */
-    public static String getDownloadPath() {
-        return downloadPath;
-    }
-
-    /**
-     * Sets the download path for the WebDriver instance
-     * TODO: Make sure this works for all browser types
-     * TODO: Figure out if we can pass the value to Saucelabs
-     * 
-     * @param downloadPath the path for all webdriver downloads
-     */
-    public static void setDownloadPath(String downloadPath) {
-        WebDriverFactory.downloadPath = downloadPath;
     }
     
     /**
