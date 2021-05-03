806abb5348899f55f1429cf119db9dfb361c182b
mmrahman <mmrahman@ets.org>
2021-05-03 10:08:43 -0400

    added unit test for grid

---
+++ src/test/java/com/dougnoel/sentinel/webdrivers/WebDriverFactoryTest.java
@@ -0,0 +1,27 @@
+package com.dougnoel.sentinel.webdrivers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WebDriverFactoryTest {
+
+	@Test(expected = org.openqa.selenium.remote.UnreachableBrowserException.class)
+	public void createGridShouldFailTest() {
+		var browser = "chrome";
+		var gridUrl = "http://gridrul.com";
+		var driver = GridWebDriverFactory.createGridDriver(browser, gridUrl);
+		 
+		assertTrue(driver == null);
+	}
+	
+	@Test(expected = org.openqa.selenium.remote.UnreachableBrowserException.class)
+	public void createGridShouldSucessTest() {
+		var browser = "chrome";
+		var gridUrl = "http://hub.technologynursery.org:4444/wd/hub";
+		var driver = GridWebDriverFactory.createGridDriver(browser, gridUrl);
+		 
+		assertTrue(driver != null);
+	}
+	
+}
