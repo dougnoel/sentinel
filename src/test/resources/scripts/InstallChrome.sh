c3e336ab8be8347c1d0c75b99d02d2f47dc6e3bf
Doug Noël <dougnoel@gmail.com>
2021-06-12 13:37:09 -0500

    Couldn't get older version to install.

--- src/test/resources/scripts/InstallChrome.sh
+++ src/test/resources/scripts/InstallChrome.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -ex
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-browser_90.0.4430.72-0ubuntu0.16.04.1_amd64.deb
-sudo apt install ./chromium-browser_90.0.4430.72-0ubuntu0.16.04.1_amd64.deb
-#sudo apt install ./google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
