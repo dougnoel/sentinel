fdc985db2a2e0995a9c7a6f8844c6a088d435926
Doug Noël <dougnoel@gmail.com>
2021-10-10 15:01:20 -0400

    Fixed unit tests that were no cleaning up after themselves.

--- src/test/java/com/dougnoel/sentinel/webdrivers/WebDriverFactoryTest.java
+++ src/test/java/com/dougnoel/sentinel/webdrivers/WebDriverFactoryTest.java
@@ -1,23 +1,34 @@
 package com.dougnoel.sentinel.webdrivers;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.dougnoel.sentinel.exceptions.SentinelException;
 
 public class WebDriverFactoryTest {
 
+	private static final String BROWSERVERSION = "browserVersion";
+	private static final String CHROME = "chrome";
+	private static final String FIREFOX = "firefox";
+	
+	@Before
+	public void setUpBeforeAnyTestsAreRun() throws SentinelException {
+		System.clearProperty(BROWSERVERSION);
+	}
+	
 	@Test(expected = org.openqa.selenium.remote.UnreachableBrowserException.class)
 	public void createGridShouldFailTest() {
-		var browser = "chrome";
 		var gridUrl = "http://gridrul.com";
-		System.setProperty("browserVersion","1234567");
-		GridWebDriverFactory.createGridDriver(browser, gridUrl);
+		System.setProperty(BROWSERVERSION,"1234567");
+		GridWebDriverFactory.createGridDriver(CHROME, gridUrl);
 	}
 	
 	@Test
 	public void createGridShouldSucceedTest() {
-		var browser = "firefox";
 		var gridUrl = "http://hub.technologynursery.org/wd/hub";
-		var driver = GridWebDriverFactory.createGridDriver(browser, gridUrl);
+		var driver = GridWebDriverFactory.createGridDriver(FIREFOX, gridUrl);
 		try {
 			assertNotNull(driver);
 		}finally {
@@ -27,10 +38,8 @@ public class WebDriverFactoryTest {
 	
 	@Test
 	public void nullBrowserVersionTest() {
-		var browser = "chrome";
 		var gridUrl = "http://hub.technologynursery.org/wd/hub";
-		System.clearProperty("browserVersion");
-		var driver = GridWebDriverFactory.createGridDriver(browser, gridUrl);
+		var driver = GridWebDriverFactory.createGridDriver(CHROME, gridUrl);
 		try {
 			assertNotNull(driver);
 		}finally {
@@ -40,11 +49,9 @@ public class WebDriverFactoryTest {
 	
 	@Test(expected = com.dougnoel.sentinel.exceptions.MalformedURLException.class)
 	public void malFormedUrlTest() {
-		var browser = "firefox";
-		System.setProperty("browserVersion","86.0");
+		System.setProperty(BROWSERVERSION,"86.0");
 		var gridUrl = "hub.technologynursery.org:4444/wd/hub";
-		GridWebDriverFactory.createGridDriver(browser, gridUrl);
-		
+		GridWebDriverFactory.createGridDriver(FIREFOX, gridUrl);
 	}
 	
 }
