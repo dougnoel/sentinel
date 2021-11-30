4c582d9c0c4e63efdbd91398fea32dbf62a4f90d
Doug Noël <dougnoel@gmail.com>
2021-11-30 12:45:19 -0500

    Fixed issues with WebDriverFactory unit tests not cleaning up after themselves. Made WebDriver.quit() safer. Deprecated PageManager.quit().

--- src/main/java/com/dougnoel/sentinel/webdrivers/WebDriverFactory.java
+++ src/main/java/com/dougnoel/sentinel/webdrivers/WebDriverFactory.java
@@ -2,6 +2,8 @@ package com.dougnoel.sentinel.webdrivers;
 
 import java.util.HashMap;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -24,6 +26,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  * For a list of supported browsers and operating systems, see the readme.
  */
 public class WebDriverFactory {
+	private static final Logger log = LogManager.getLogger(WebDriverFactory.class);
     private static WebDriver driver = null;
     private static WebDriverFactory instance = null;
     private static String parentHandle = null;
@@ -74,9 +77,6 @@ public class WebDriverFactory {
         	WebDriverManager.edgedriver().setup();
         	driver = new EdgeDriver();
         	break;
-        case "customChrome":
-        	driver = createCustomChromeDriver();
-            break;
         case "firefox":
     		WebDriverManager.firefoxdriver().setup();
     		driver = new FirefoxDriver();
@@ -131,8 +131,17 @@ public class WebDriverFactory {
      * Quits the driver and sets the driver instance back to null.
      */
     public static void quit() {
-    	getWebDriver().quit();
-    	driver = null;
+    	if (exists()) {
+    		getWebDriver().quit();
+    		driver = null;
+    	}
+    	else {
+    		log.info("Attempted to call quit on a driver that did not exist.");
+    	}
+    }
+    
+    public static boolean exists() {
+    	return driver != null;
     }
     
     /**
@@ -164,17 +173,9 @@ public class WebDriverFactory {
     		chromeOptions.addArguments("--disable-dev-shm-usage");
     		chromeOptions.addArguments("--headless");        		
     	}
-    	WebDriverManager.chromedriver().setup();
-    	return new ChromeDriver(chromeOptions);
-    }
-    
-    /**
-     * Creates a ChromeDriver using a custom Chrome executable. The path for that executable must be set using the -DchromeBrowserBinary configuration option.
-     * @return WebDriver ChromeDrvier
-     */
-    private static WebDriver createCustomChromeDriver() {
-    	var chromeOptions = new ChromeOptions();
-    	chromeOptions.setBinary(Configuration.toString("chromeBrowserBinary"));
+    	var binary = Configuration.toString("chromeBrowserBinary");
+    	if (binary != null)
+    		chromeOptions.setBinary(binary);
     	WebDriverManager.chromedriver().setup();
     	return new ChromeDriver(chromeOptions);
     }
--- src/test/java/com/dougnoel/sentinel/webdrivers/WebDriverFactoryTest.java
+++ src/test/java/com/dougnoel/sentinel/webdrivers/WebDriverFactoryTest.java
@@ -1,28 +1,41 @@
 package com.dougnoel.sentinel.webdrivers;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
-import com.dougnoel.sentinel.exceptions.SentinelException;
+import com.dougnoel.sentinel.configurations.Configuration;
 
 public class WebDriverFactoryTest {
 
 	private static final String BROWSERVERSION = "browserVersion";
-	private static final String CHROME = "chrome";
 	private static final String FIREFOX = "firefox";
+	private static final String GRIDURL = "gridUrl";
+	private static final String BROWSER = "browser";
 	
-	@Before
-	public void setUpBeforeAnyTestsAreRun() throws SentinelException {
+//	@BeforeClass
+//	public static void setUpBeforeAnyTestsAreRun() throws SentinelException {
+//		System.clearProperty(BROWSERVERSION);
+//	}
+	
+	@After
+	public void tearDownAfterEachTest() throws Exception {
+		Configuration.clear(BROWSERVERSION);
 		System.clearProperty(BROWSERVERSION);
+		Configuration.clear(GRIDURL);
+		System.clearProperty(GRIDURL);
+		Configuration.clear(BROWSER);
+		System.clearProperty(BROWSER);
+		Configuration.clear("chromeBrowserBinary");
+		System.clearProperty("chromeBrowserBinary");
+		
+		WebDriverFactory.quit();
 	}
 	
 	@Test(expected = org.openqa.selenium.remote.UnreachableBrowserException.class)
 	public void createGridShouldFailTest() {
-		var gridUrl = "http://gridrul.com";
 		System.setProperty(BROWSERVERSION,"1234567");
-		GridWebDriverFactory.createGridDriver(CHROME, gridUrl);
+		System.setProperty(GRIDURL,"http://gridrul.com");
+		WebDriverFactory.instantiateWebDriver();
 	}
 
 // TODO: Selenium Grid not working. Need to setup a self-contained one for testing.
@@ -50,9 +63,15 @@ public class WebDriverFactoryTest {
 	
 	@Test(expected = com.dougnoel.sentinel.exceptions.MalformedURLException.class)
 	public void malFormedUrlTest() {
+		System.setProperty(BROWSER, FIREFOX);
 		System.setProperty(BROWSERVERSION,"86.0");
-		var gridUrl = "hub.technologynursery.org:4444/wd/hub";
-		GridWebDriverFactory.createGridDriver(FIREFOX, gridUrl);
+		System.setProperty(GRIDURL,"hub.technologynursery.org:4444/wd/hub");
+		WebDriverFactory.instantiateWebDriver();
 	}
 	
+	@Test(expected = org.openqa.selenium.WebDriverException.class)
+	public void createCustomChromeDriver() {
+		System.setProperty("chromeBrowserBinary","fake/path");
+		WebDriverFactory.instantiateWebDriver();
+	}
 }
