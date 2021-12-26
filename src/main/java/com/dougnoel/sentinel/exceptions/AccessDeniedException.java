9714961a27224ac7b1dd9c6be8187e7fb6507396
Doug Noël <dougnoel@gmail.com>
2021-12-26 16:43:58 -0500

    Added logging to AccessDeniedException.

--- src/main/java/com/dougnoel/sentinel/exceptions/AccessDeniedException.java
+++ src/main/java/com/dougnoel/sentinel/exceptions/AccessDeniedException.java
@@ -2,6 +2,9 @@ package com.dougnoel.sentinel.exceptions;
 
 import java.io.File;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Creating an unchecked AccessDeniedException so that we can fail a test without failing the entire
  * program execution.
@@ -10,27 +13,32 @@ import java.io.File;
  *
  */
 public class AccessDeniedException extends SentinelException {
+	private static final Logger log = LogManager.getLogger(AccessDeniedException.class);
     private static final long serialVersionUID = 8461275760821180367L;
     private final File filePath;
 
     public AccessDeniedException(File filePath) {
     	super();
         this.filePath = filePath;
+        log.error(this.getMessage(), this);
     }
     
     public AccessDeniedException(Throwable cause, File filePath) {
         super(cause);
         this.filePath = filePath;
+        log.error(this.getMessage(), this);
     }
     
     public AccessDeniedException(String message, File filePath) {
         super(message);
         this.filePath = filePath;
+        log.error(this.getMessage(), this);
     }
 
     public AccessDeniedException(String message, Throwable cause, File filePath) {
         super(message, cause);
         this.filePath = filePath;
+        log.error(this.getMessage(), this);
     }
     
     @Override
