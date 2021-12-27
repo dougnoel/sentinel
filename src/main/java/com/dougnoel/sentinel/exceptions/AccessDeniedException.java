647e039e62d45d62e2977c5f55b13265efade716
Doug Noël <dougnoel@gmail.com>
2021-12-27 00:11:14 -0500

    Updated Javadocs.

--- src/main/java/com/dougnoel/sentinel/exceptions/AccessDeniedException.java
+++ src/main/java/com/dougnoel/sentinel/exceptions/AccessDeniedException.java
@@ -17,39 +17,78 @@ public class AccessDeniedException extends SentinelException {
     private static final long serialVersionUID = 8461275760821180367L;
     private final File file;
 
-    public AccessDeniedException(File filePath) {
+    /**
+     * Creates a custom exception message and logs it to the error log.
+     * 
+     * @param file java.io.File the File that caused the exception
+     */
+    public AccessDeniedException(File file) {
     	super();
-        this.file = filePath;
+        this.file = file;
         log.error(this.getMessage(), this);
     }
     
-    public AccessDeniedException(Throwable cause, File filePath) {
+    /**
+     * Creates a custom exception message and logs it to the error log.
+     * 
+     * @param cause Throwable the exception that was thrown that caused this exception to be thrown
+     * @param file java.io.File the File that caused the exception
+     */
+    public AccessDeniedException(Throwable cause, File file) {
         super(cause);
-        this.file = filePath;
+        this.file = file;
         log.error(this.getMessage(), this);
     }
     
-    public AccessDeniedException(String message, File filePath) {
+    /**
+     * Creates a custom exception message and logs it to the error log.
+     * 
+     * @param message String the custom text to add to the custom exception message
+     * @param file java.io.File the File that caused the exception
+     */
+    public AccessDeniedException(String message, File file) {
         super(message);
-        this.file = filePath;
+        this.file = file;
         log.error(this.getMessage(), this);
     }
 
-    public AccessDeniedException(String message, Throwable cause, File filePath) {
+    /**
+     * Creates a custom exception message and logs it to the error log.
+     * 
+     * @param message String the custom text to add to the custom exception message
+     * @param cause Throwable the exception that was thrown that caused this exception to be thrown
+     * @param file java.io.File the File that caused the exception
+     */
+    public AccessDeniedException(String message, Throwable cause, File file) {
         super(message, cause);
-        this.file = filePath;
+        this.file = file;
         log.error(this.getMessage(), this);
     }
     
+    /**
+     * Returns a custom error message.
+     * 
+     * @return String the custom error message
+     */
     @Override
     public String getMessage() {
     	return super.getMessage() + " Access denied for file path: " + filePath();
     }
     
+    /**
+     * Returns the File set when this exception was created.
+     * 
+     * @return java.io.File the File that caused the exception
+     */
 	public File getFile() {
     	return file;
     }
 	
+	/**
+	 * Returns the absolute path to the file or directory that threw the error.
+	 * 
+	 * @return String the absolute path of the File that caused the exception
+	 */
 	public String filePath() {
 		return file.getAbsoluteFile().toString();
 	}
