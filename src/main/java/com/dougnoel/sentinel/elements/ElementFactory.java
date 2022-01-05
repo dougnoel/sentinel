4d338196b2e6cd4a437bd81d5d7d14379b81fa1b
Sam Pacos <spacos@metabolon.com>
2022-01-05 16:31:00 -0500

    Small logic changes in ElementFactory. Added comments in createElement method in ElementFactory

--- src/main/java/com/dougnoel/sentinel/elements/ElementFactory.java
+++ src/main/java/com/dougnoel/sentinel/elements/ElementFactory.java
@@ -15,9 +15,17 @@ import org.openqa.selenium.NoSuchElementException;
 
 public class ElementFactory {
     
-    protected static Map<String,Class> elementClasses = new HashMap<>();
+    protected static Map<String,Class<?>> elementClasses = new HashMap<>();
 
-
+    /**
+     * Returns an Object that is an Element using the element name and Page. 
+     * We look first in the Sentinel jar, then in the local repository class files.
+     * If we don't find the element type in either location, we create a base Element type.
+     * 
+     * @param elementName String the name of the element to create
+     * @param page Page the page object to lookup the element from
+     * @return Object the element that is created
+     */
 	public static Object createElement(String elementName, Page page) {
 		Map<String, String> elementData = findElement(elementName, page.getName());
 		
@@ -33,24 +41,33 @@ public class ElementFactory {
 			
 			elementType = elementData.get("elementType");
 			
-			Class mappedAndRetrievedClass = elementClasses.get(elementType);
-			if (mappedAndRetrievedClass != null)
+			Class<?> mappedAndRetrievedClass;
+
+            // If the cached Class of the elementType has already been found, create an element of that Class
+			if ((mappedAndRetrievedClass = elementClasses.get(elementType)) != null)
 				return mappedAndRetrievedClass.getConstructor(String.class, Map.class).newInstance(elementName, elementData);
 			
+            // Attempt to look in com.dougnoel.sentinel.elements for the Class corresponding to the elementType
+            // This will work in projects extending Sentinel for element types in the Sentinel jar.
 			mappedAndRetrievedClass = retrieveClassBySimpleName(elementType);
 			
+            // If this ^^^ attempt failed, look on disk
 			if (mappedAndRetrievedClass == null) {
+                // First, look for the Class in "this" project's directories. This is the case where a custom element type is created in a project extending Sentinel.
 				String classPath = Configuration.getClassPath(elementType);
 		    	if (classPath == null) {
+                    // If the above search (in project directories) fails, default the class to "Element".
 		    		mappedAndRetrievedClass = Element.class;
-		    		
 		    	} else {
+                    // If the above search is successful, fetch the Class corresponding to the elementType.
 		    		mappedAndRetrievedClass = Class.forName(classPath);
 		    	}
 			}
 	    	
+            // Add the resolved Class of the elementType to the elementClasses HashMap so the next time an element of that type is created we have the Class cached.
 	    	elementClasses.put(elementType, mappedAndRetrievedClass);
 	    	
+            // Fetch the constructor for the resolved Class and call it to create a new instance of the element.
 	    	return mappedAndRetrievedClass.getConstructor(String.class, Map.class).newInstance(elementName, elementData);
 
         }catch(NoSuchMethodException|InvocationTargetException|IllegalAccessException|InstantiationException|ClassNotFoundException e){
@@ -58,7 +75,12 @@ public class ElementFactory {
         }
 	}
 	
-    private static Class retrieveClassBySimpleName(String elementType){
+    /**
+     * 
+     * @param elementType
+     * @return
+     */
+    private static Class<?> retrieveClassBySimpleName(String elementType){
         try{
             var allClasses = ClassPath.from(ClassLoader.getSystemClassLoader())
                 .getTopLevelClassesRecursive("com.dougnoel.sentinel.elements")
@@ -75,6 +97,12 @@ public class ElementFactory {
         }
     }
 
+    /**
+     * 
+     * @param elementName
+     * @param pageName
+     * @return
+     */
     private static Map<String, String> findElement(String elementName, String pageName) {
 		Map<String, String> elementData = Configuration.getElement(elementName, pageName);
 		if (elementData == null) {
