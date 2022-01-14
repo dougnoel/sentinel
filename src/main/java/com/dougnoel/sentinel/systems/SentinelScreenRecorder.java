b100cfd7cf6983e83d9bcb5d661d6307ed38d7c0
Sam Pacos <spacos@metabolon.com>
2022-01-14 12:15:04 -0500

    specify log location in recorder and artifact upload step

--- src/main/java/com/dougnoel/sentinel/systems/SentinelScreenRecorder.java
+++ src/main/java/com/dougnoel/sentinel/systems/SentinelScreenRecorder.java
@@ -1,6 +1,8 @@
 package com.dougnoel.sentinel.systems;
 
 import java.awt.*;
+import java.io.File;
+
 import org.monte.media.Format;
 import org.monte.media.math.Rational;
 import org.monte.screenrecorder.ScreenRecorder;
@@ -14,12 +16,14 @@ public class SentinelScreenRecorder {
 
 		GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
 				.getDefaultConfiguration();
-		screenRecorder = new ScreenRecorder(gc, new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
+		screenRecorder = new ScreenRecorder(gc, gc.getBounds(), 
+				new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
 				new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
 						CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24, FrameRateKey,
 						Rational.valueOf(15), QualityKey, 1.0f, KeyFrameIntervalKey, 15 * 60),
 				new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey, Rational.valueOf(30)),
-				null);
+				null,
+				new File("./reports/"));
 		screenRecorder.start();
 
 	}
