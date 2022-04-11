91efc7a2a21a75117c6e9879e74495a06a4f9372
Doug Noel <dougnoel@gmail.com>
2022-04-11 19:09:05 -0400

    Added assertion to Windows maximize test.

--- src/test/java/com/dougnoel/sentinel/webdrivers/DriverTests.java
+++ src/test/java/com/dougnoel/sentinel/webdrivers/DriverTests.java
@@ -1,6 +1,9 @@
 package com.dougnoel.sentinel.webdrivers;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
 
 import com.dougnoel.sentinel.pages.PageManager;
 
@@ -9,8 +12,9 @@ public class DriverTests {
 	@Test
 	public void MaximizeWindowTest() {
 		PageManager.setPage("CorrectPageObject");
-		Driver.getWebDriver();
+		var js = (JavascriptExecutor)Driver.getWebDriver();
 		Driver.maximizeWindow();
+		assertTrue("Expecting window to be maximized.", js.executeScript("return document.hidden").toString() == "false");
 		Driver.quitAllDrivers();
 	}
 
