f5d466ca6650a6bd7080c18350436706d954434b
Tyler Bouchard <tbouchard@metabolon.com>
2022-04-22 16:51:28 -0400

    Filenames have been reworked so that each scenario now has it's own easier to locate folder. Comparisons will be sorted by page and element with the post-text indicating expected, actual, and result. Failure status will be implemented later if desired.
    To support windows terminology the term "window" is now checked for with regards to full app screenshots.
    
    Code has been updated to use .Driver rather than webdriverfactory.

--- src/main/java/com/dougnoel/sentinel/steps/ImageSteps.java
+++ src/main/java/com/dougnoel/sentinel/steps/ImageSteps.java
@@ -5,12 +5,12 @@ import static com.dougnoel.sentinel.elements.ElementFunctions.getElement;
 import java.io.File;
 import java.io.IOException;
 
+import com.dougnoel.sentinel.webdrivers.Driver;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import com.dougnoel.sentinel.system.FileManager;
 import com.dougnoel.sentinel.pages.PageManager;
-import com.dougnoel.sentinel.webdrivers.WebDriverFactory;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -27,22 +27,23 @@ public class ImageSteps {
 	/**
 	 * Takes a screenshot of the given element and stores it for later comparison
 	 * @param elementName String name of the element to screenshot
-	 * @throws IOException
+	 * @throws IOException If the expected image to be created fails to save
 	 */
-    @When("^I take a screenshot of (?:the|a) (page|.*)$")
+    @When("^I take a screenshot of (?:the|a) (.*?)$")
     public static void storeScreenshotOfElement(String elementName) throws IOException {
-    	String imageFileName = scenario.getName()+ "_" + elementName + "_" + PageManager.getPage().getName() + ".png";
+		String outputFolder = "imageComparison/" + scenario.getName();
+    	String imageFileName = PageManager.getPage().getName() + "_" + elementName + "_EXPECTED" + ".png";
     	File screenshotFile;
 
     	//Determine if we're taking a screenshot of an element or the whole page.
-    	if(!elementName.toLowerCase().contentEquals("page")) {
+    	if(!elementName.toLowerCase().contentEquals("page") && !elementName.toLowerCase().contentEquals("window")) {
     		screenshotFile = getElement(elementName).getScreenshot();
     	}
     	else {
-        	TakesScreenshot pageScreenshotTool =((TakesScreenshot) WebDriverFactory.getWebDriver());
+        	TakesScreenshot pageScreenshotTool =((TakesScreenshot) Driver.getWebDriver());
         	screenshotFile = pageScreenshotTool.getScreenshotAs(OutputType.FILE);
     	}
     	
-    	FileManager.saveImage("expected", imageFileName, screenshotFile);
+    	FileManager.saveImage(outputFolder, imageFileName, screenshotFile);
     }
 }
