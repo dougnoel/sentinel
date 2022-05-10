88c810b1dbcc102db3df0709715596ff3af2c05d
Tyler Bouchard <tbouchard@metabolon.com>
2022-05-10 17:42:56 -0400

    -Changed to the correct logger -Removed scenario access as it is no longer needed -Removed throws -Cleaned imports

--- src/main/java/com/dougnoel/sentinel/steps/ImageSteps.java
+++ src/main/java/com/dougnoel/sentinel/steps/ImageSteps.java
@@ -4,9 +4,7 @@ import static com.dougnoel.sentinel.elements.ElementFunctions.getElement;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
-import com.dougnoel.sentinel.elements.Element;
 import com.dougnoel.sentinel.enums.PageObjectType;
 import com.dougnoel.sentinel.webdrivers.Driver;
 import org.apache.logging.log4j.LogManager;
@@ -17,26 +15,17 @@ import org.openqa.selenium.TakesScreenshot;
 import com.dougnoel.sentinel.system.FileManager;
 import com.dougnoel.sentinel.pages.PageManager;
 
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.cucumber.java.en.When;
 
 public class ImageSteps {
-	private static Scenario scenario;
-	protected static final Logger log = LogManager.getLogger(Element.class.getName()); // Create a logger.
-
-	@Before
-    public static void before(Scenario scenario) {
-		ImageSteps.scenario = scenario;
-	}
+	protected static final Logger log = LogManager.getLogger(ImageSteps.class.getName()); // Create a logger.
 	
 	/**
 	 * Takes a screenshot of the given element and stores it for later comparison
 	 * @param elementName String name of the element to screenshot
-	 * @throws IOException If the expected image to be created fails to save
 	 */
     @When("^I take a screenshot of (?:the|a) (page|window|.*?)$")
-    public static void storeScreenshotOfElement(String elementName) throws IOException {
+    public static void storeScreenshotOfElement(String elementName) {
     	String imageFileName = "tempTocompare" + ".png";
     	File screenshotFile;
 		PageObjectType pageType = PageManager.getCurrentPageObjectType();
