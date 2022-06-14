6808669c724f92cce6c1e6164acee33843f3fbbd
Tyler Bouchard <tbouchard@metabolon.com>
2022-06-14 16:48:17 -0400

    Overrode the wait for text within the text box class as they require the text to be present in element value.

--- src/main/java/com/dougnoel/sentinel/elements/Textbox.java
+++ src/main/java/com/dougnoel/sentinel/elements/Textbox.java
@@ -1,5 +1,13 @@
 package com.dougnoel.sentinel.elements;
 
+import com.dougnoel.sentinel.configurations.Time;
+import com.dougnoel.sentinel.webdrivers.Driver;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.Map;
 
 /**
@@ -26,5 +34,32 @@ public class Textbox extends Element {
 	@Override
 	public String getText() {
 		return element().getAttribute("value");
+	}
+
+	/**
+	 * Waits until the text contains a certain value, and returns if it was found
+	 *
+	 * @return Boolean If the text value was found in the element.
+	 */
+	@Override
+	public Boolean waitForText(String text, boolean present) {
+		ExpectedCondition<Boolean> condition = ExpectedConditions.textToBePresentInElementValue(element(), text);
+		if (!present)
+			condition = ExpectedConditions.not(ExpectedConditions.textToBePresentInElementValue(element(), text));
+
+		long searchTime = Time.out().getSeconds() * 1000;
+		long startTime = System.currentTimeMillis(); // fetch starting time
+
+		while ((System.currentTimeMillis() - startTime) < searchTime) {
+			try {
+				return new WebDriverWait(Driver.getWebDriver(), Time.interval().toMillis(), Time.loopInterval().toMillis())
+						.ignoring(StaleElementReferenceException.class)
+						.ignoring(TimeoutException.class)
+						.until(condition);
+			} catch (TimeoutException e) {
+				// suppressing this due to falsely thrown timeout exception
+			}
+		}
+		return false;
 	}
 }
