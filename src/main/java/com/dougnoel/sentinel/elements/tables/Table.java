307c6c5d672cb8a6e34cdc535745e281cac0e0fa
Sam Pacos <spacos@metabolon.com>
2022-09-29 12:11:18 -0400

    fix javadoc

--- src/main/java/com/dougnoel/sentinel/elements/tables/Table.java
+++ src/main/java/com/dougnoel/sentinel/elements/tables/Table.java
@@ -744,9 +744,11 @@ public class Table extends Element {
 	}
 
 	/**
-	 * Returns true if column is equal, false if column is not equal
+	 * Returns true if the table has a column header that's equal to the given text. false if the table does not have a column header that's equal to the given text.
 	 *
 	 * @param columnName String name of column to find
+	 * @param partialMatch boolean if true, this method returns null if the actual header contains the given columnHeader.
+	 * 	 *                     if false, this method returns null when the actual header exactly matches the given columnHeader.
 	 * @return boolean true if column equals, false if column is not equal.
 	 */
 	public boolean verifyColumnHeaderEquals(String columnName, boolean partialMatch) {
