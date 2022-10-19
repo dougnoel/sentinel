0b850dc1e33ff5576d6623c365f901058601dfe7
Paul Turchinetz <pturchinetz@metabolon.com>
2022-10-19 15:57:22 -0400

    Adding unit test

---
+++ src/test/java/com/dougnoel/sentinel/webdrivers/WindowListTest.java
@@ -0,0 +1,36 @@
+package com.dougnoel.sentinel.webdrivers;
+
+import com.dougnoel.sentinel.configurations.Configuration;
+import com.dougnoel.sentinel.configurations.Time;
+import com.dougnoel.sentinel.steps.BaseSteps;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.dougnoel.sentinel.webdrivers.Driver.doesWindowExist;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WindowListTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        Time.reset();
+        Configuration.update("timeout", 1);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        Time.reset();
+        Configuration.update("timeout", 3);
+        Driver.quitAllDrivers();
+    }
+
+    @Test
+    public void checkWindowExists() {
+        BaseSteps.navigateToPage("InternetPage");
+        assertTrue("Expecting window exists.", doesWindowExist("The Internet"));
+        assertFalse("Expecting window does not exist.", doesWindowExist("Lorem"));
+    }
+
+}
