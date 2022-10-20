1b5c1b44343f82ba1b166132a6a034dd49bcd51a
Paul Turchinetz <pturchinetz@metabolon.com>
2022-10-20 14:33:09 -0400

    Unused import

--- src/test/java/com/dougnoel/sentinel/webdrivers/DriverTests.java
+++ src/test/java/com/dougnoel/sentinel/webdrivers/DriverTests.java
@@ -2,7 +2,6 @@ package com.dougnoel.sentinel.webdrivers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.openqa.selenium.JavascriptExecutor;
